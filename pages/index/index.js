155ecdfdba3c288f637c98d0d95da603c9297f95
jiangnan <229142144@qq.com>
2018-05-28 18:46:33 +0800

    merge

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,9 +4,44 @@ const app = getApp()
 
 Page({
     data: {
-        
+        imgUrls: [
+            'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+            'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+            'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+        ],
+        indicatorDots: true,
+        indicatorColor: 'rgba(0, 0, 0, .3)',
+        indicatorActiveColor: '#ff5977',
+        autoplay: false,
+        interval: 5000,
+        duration: 1000,
+        recommendIndex: 0
     },
-    
+    changeIndicatorDots: function(e) {
+        this.setData({
+            indicatorDots: !this.data.indicatorDots
+        })
+    },
+    changeAutoplay: function(e) {
+        this.setData({
+            autoplay: !this.data.autoplay
+        })
+    },
+    intervalChange: function(e) {
+        this.setData({
+            interval: e.detail.value
+        })
+    },
+    durationChange: function(e) {
+        this.setData({
+            duration: e.detail.value
+        })
+    },
+    changeRecommend: function(e) {
+        this.setData({
+            recommendIndex: +e.currentTarget.id
+        })
+    },
     onLoad: function () {
         
     }
