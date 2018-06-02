f8c11141b52ac12c536cfec4d627d6737d08ab43
jiangnan <229142144@qq.com>
2018-06-02 11:50:33 +0800

    me

---
+++ pages/me/me.js
@@ -0,0 +1,31 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+    data: {
+    	me: {},
+		list: [
+			{icon: '/assets/images/recharge.png', name: '充值', link: ''},
+			{icon: '/assets/images/recharge_record.png', name: '充值记录', link: ''},
+			{icon: '/assets/images/pay_record.png', name: '消费记录', link: ''},
+			{icon: '/assets/images/qa.png', name: '常见问题', link: ''},
+			{icon: '/assets/images/opinion.png', name: '意见反馈', link: ''}
+		]
+    },
+    
+    onLoad: function () {
+		this.setData({
+			me: {name: 'jn', money: '999999', id: '223344'}
+		})
+    },
+    tap: function(e) {
+    	console.log(this.data.showAnswer)
+    	const index = e.currentTarget.id
+    	const arr = this.data.showAnswer
+    	arr[index] = !this.data.showAnswer[index]
+    	this.setData({
+    		showAnswer: arr
+    	})
+    }
+})
