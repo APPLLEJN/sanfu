791f85552453790e58922a5a17cbf61b4305edc6
sunchi <miaomiao8890@vip.qq.com>
2018-06-03 22:36:52 +0800

    反馈页面

--- pages/me/me.js
+++ pages/me/me.js
@@ -10,7 +10,7 @@ Page({
 			{icon: '/assets/images/recharge_record.png', name: '充值记录', link: '/pages/record/record?type=recharge'},
 			{icon: '/assets/images/pay_record.png', name: '消费记录', link: '/pages/record/record?type=pay'},
 			{icon: '/assets/images/qa.png', name: '常见问题', link: '/pages/qa/qa'},
-			{icon: '/assets/images/opinion.png', name: '意见反馈', link: ''}
+      { icon: '/assets/images/opinion.png', name: '意见反馈', link: '/pages/feedback/feedback'}
 		]
     },
     
