f0e09ac378fdd4c19beb5e43b1855a71430a9a02
sunchi <miaomiao8890@vip.qq.com>
2018-06-04 16:24:22 +0800

    新增充值页h5

---
+++ h5/js/main.js
@@ -0,0 +1,21 @@
+var isDev = true;
+
+var Main = {
+  
+  _initDom: function() {
+    var _this = this
+
+    
+  },
+  _bindEvent: function() {
+    var _this = this;
+
+  },
+  init: function() {
+    this._initDom();
+    this._bindEvent();
+  }
+}
+
+// run
+Main.init()
