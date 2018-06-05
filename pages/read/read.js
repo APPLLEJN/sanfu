7e3cb8ae94590fdb006d6a89164c9a42ec8adbab
jiangnan <229142144@qq.com>
2018-06-05 21:08:28 +0800

    merge

---
+++ pages/read/read.js
@@ -0,0 +1,40 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+    data: {
+		top:0,
+		scrollTop: 0,
+		height: wx.getSystemInfoSync().windowHeight,
+		width: wx.getSystemInfoSync().windowWidth
+	},
+    
+    onLoad: function () {
+		this.setData({
+			me: {name: 'jn', money: '999999', id: '223344'}
+		})
+    },
+    tap: function(e) {
+		const {scrollTop, height, width} = this.data
+		const left = width/3
+		const right = width/3*2
+		const x = e.detail.x
+		let top
+		if (x< left) {
+			top = scrollTop - height
+		} else if (x > right) {
+			top = scrollTop + height
+		} else {
+			console.log('center')
+		}
+		this.setData({
+			top: top
+		})
+    },
+	scroll: function(e) {
+		this.setData({
+			scrollTop: e.detail.scrollTop
+		})
+	}
+})
