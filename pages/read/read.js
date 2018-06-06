032d2bc5477dcea580ea6e1029ba339a3addd3b9
jiangnan <229142144@qq.com>
2018-06-06 21:23:58 +0800

    lazy load

--- pages/read/read.js
+++ pages/read/read.js
@@ -6,14 +6,62 @@ Page({
     data: {
 		scrollTop: 0,
 		height: wx.getSystemInfoSync().windowHeight,
-		width: wx.getSystemInfoSync().windowWidth
+		width: wx.getSystemInfoSync().windowWidth,
+		imageList: [
+			'http://www.pujia8.com/static/pics/20170930120018_57.jpg',
+			'http://www.pujia8.com/static/uploads/20180516141341_49.jpg',
+			'http://www.pujia8.com/static/uploads/20180516141343_89.jpg',
+			'http://www.pujia8.com/static/uploads/20180516141346_15.jpg',
+			'http://www.pujia8.com/static/uploads/20180516141349_44.jpg',
+			'http://www.pujia8.com/static/uploads/20180516141351_90.jpg',
+			'http://www.pujia8.com/static/uploads/20180606100423_51.png',
+		],
+		arr: [],
+		arrHeight: [],
+		itemHeight: 0,
 	},
     
     onLoad: function () {
 		this.setData({
 			me: {name: 'jn', money: '999999', id: '223344'}
 		})
+		const {imageList,arr} = this.data
+		imageList.map(item => {
+			arr.push(false)
+		})
+		this.setData({
+			arr: arr
+		})
     },
+	onReady: function () {
+		setTimeout(() => {
+			this.getRect()
+		}, 1000)
+	},
+	getRect: function () {
+		var that = this
+		wx.createSelectorQuery().select('.image').boundingClientRect(function(rect) {
+			that.setData({
+				itemHeight: rect.height,
+			})
+			console.log(rect.height)
+			that.init(rect.height)
+		}).exec()
+	},
+	init: function (itemHeight) {
+		const { height } = this.data
+		let index = parseFloat(height/itemHeight)
+		for (let i = 0;i < index; i++) {
+			this.data.arr[i] = true
+		}
+		this.setData({
+			arr: this.data.arr
+		})
+		for (let i = 0;i < this.data.arr.length; i++) {
+			this.data.arrHeight[i] = Math.floor(i/2) * (itemHeight + 10)
+		}
+
+	},
     tap: function(e) {
 		const {scrollTop, height, width} = this.data
 		const left = width/3
@@ -32,8 +80,19 @@ Page({
 		})
     },
 	scroll: function(e) {
+		const {arr, arrHeight, height} = this.data
+		console.log(arrHeight)
+		for (var i = 0; i < arrHeight.length; i++) {
+			if (arrHeight[i] < e.detail.scrollTop + height) {
+				if (arr[i] == false) {
+					arr[i] = true;
+				}
+			}
+		}
+		console.log(arr, '===')
 		this.setData({
-			scrollTop: e.detail.scrollTop
+			scrollTop: e.detail.scrollTop,
+			arr: this.data.arr
 		})
 	}
 })
