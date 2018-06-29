25ae23d94efe14d066cf1454d94d0e4dc58fdf77
sunchi <miaomiao8890@vip.qq.com>
2018-06-30 00:17:16 +0800

    修复bug

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -5,14 +5,17 @@ const app = getApp()
 
 Page({
     data: {
-		info: {},
-		tabOn: 0,
-		order: 'down',
-    	isCollected: false,
-		id: null,
-		readCurrent: 1,
-		directoryName: '',
-    	isBgShow: false
+      info: {},
+      tabOn: 0,
+      order: 'down',
+      isCollected: false,
+      id: null,
+      readCurrent: 1,
+      directoryName: '',
+    	isBgShow: false,
+      click_cnt: '0',
+      like_cnt: '0',
+      fav_cnt: '0'
     },
     
     onLoad: function (option) {
@@ -44,13 +47,42 @@ Page({
 					title: result.data.comic_name
 				})
 				this.setData({
-                    isCollected: result.data.has_faved,
+          isCollected: result.data.has_faved,
 					comic: result.data,
-          			chapters: result.data.chapters,
-                    readCurrent: result.data.last_read_chapter_id,
+          chapters: result.data.chapters,
+          readCurrent: result.data.last_read_chapter_id,
 					directoryName: result.data.chapters.filter(item=>item.chapter_id==result.data.last_read_chapter_id)[0].title,
 					isBgShow: true
 				})
+
+        if (this.data.comic.click_cnt && this.data.comic.click_cnt > 10000) {
+          this.setData({
+            click_cnt: (this.data.comic.click_cnt/10000).toFixed(1)
+          })
+        } else {
+          this.setData({
+            click_cnt: this.data.comic.click_cnt
+          })
+        }
+        if (this.data.comic.like_cnt && this.data.comic.like_cnt > 10000) {
+          this.setData({
+            like_cnt: (this.data.comic.like_cnt / 10000).toFixed(1)
+          })
+        } else {
+          this.setData({
+            like_cnt: this.data.comic.like_cnt
+          })
+        }
+        if (this.data.comic.fav_cnt && this.data.comic.fav_cnt > 10000) {
+          this.setData({
+            fav_cnt: (this.data.comic.fav_cnt / 10000).toFixed(1)
+          })
+        } else {
+          this.setData({
+            fav_cnt: this.data.comic.fav_cnt
+          })
+        }
+        
 			}
 		})
 	},
